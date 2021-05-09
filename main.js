17540fe3037115c4f712f2b91e3e2858fdfe7206
non0829 <u474334e@ecs.osaka-u.ac.jp>
2021-05-09 16:53:24 +0900

    portfolio 完成

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
+const html = document.querySelector('html')
 const btn = document.getElementById('btn')
+const reset = document.getElementById('reset')
+const changeColor = document.getElementById('changeColor')
 const span = document.querySelectorAll('span')
 const links = document.querySelectorAll('header li')
 const circles = document.querySelectorAll('.circle');
 
 btn.addEventListener('click', () => {
-  console.log('aaa')
   span.forEach((stick, index) => {
     stick.classList.toggle(`stick${index + 1}`)
   })
@@ -14,11 +16,33 @@ btn.addEventListener('click', () => {
 });
 
 
+
 circles.forEach((circle) => {
   circle.addEventListener('click', () => {
-    circle.classList.toggle('display')
-  })
+    let hue = Math.floor(Math.random() * 361);
+    let saturation = 80 + Math.floor(Math.random() * 20);
+    let lightness = 50 + Math.floor(Math.random() * 30);
+    circle.style.backgroundColor = `hsl(${hue}, ${saturation}%, ${lightness}%)`;
+  });
 });
 
+reset.addEventListener('click', () => {
+  circles.forEach((circle) => {
+    circle.style.backgroundColor = '#fff'
+    circle.style.boxShadow = "4px 4px 10px rgba(176, 196, 222, 1)"
+    html.style.backgroundColor =  'hsla(180, 30%, 80%)';
+  })
+  
+})
+
+changeColor.addEventListener('click', () => {
+  let saturation = 80 + Math.floor(Math.random() * 20);
+  let lightness = 50 + Math.floor(Math.random() * 30);
+  let hue = Math.floor(Math.random() * 361);
+  circles.forEach((circle) => {
+    circle.style.backgroundColor = `hsl(${hue}, ${saturation}%, ${lightness}%)`
+    html.style.backgroundColor = `hsl(${hue + 60}, ${saturation - 20}%, ${lightness + 10}%)`
+  })
+})
 
 
